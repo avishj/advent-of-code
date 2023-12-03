c9bbbed4059b64b29f6d8986e838b752ab3e193c
Avish Jha <avish.j@protonmail.com>
2023-12-03 19:44:06 +0530

    rename q1 solution

--- Day 1/SolutionQ1.java
+++ Day 1/SolutionQ1.java
@@ -2,7 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class Solution {
+public class SolutionQ1 {
 
     private enum Direction {
         LEFT,
