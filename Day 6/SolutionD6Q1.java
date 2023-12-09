5c53bf5a250405a2f3a22f93b103f9fe6771119c
Avish Jha <avish.j@protonmail.com>
2023-12-09 17:04:36 +0530

    further optimised code

--- Day 6/SolutionD6Q1.java
+++ Day 6/SolutionD6Q1.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.UnaryOperator;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -31,22 +32,23 @@ public class SolutionD6Q1 {
         return null;
     }
 
+    private static Optional<Integer> calculateRange(Integer key, Integer value, UnaryOperator<Integer> mapFunction) {
+        return IntStream.range(0, key)
+                .boxed()
+                .map(mapFunction)
+                .filter(i -> value < i * (key - i))
+                .findFirst();
+    }
+
     public static void main(String[] args) {
         Map<Integer, Integer> inputs = processInput();
         AtomicLong result = new AtomicLong(1);
         inputs.entrySet().forEach(entry -> {
-            Optional<Integer> startWin = IntStream.range(0, entry.getKey())
-                    .boxed()
-                    .filter(i -> entry.getValue() < i * (entry.getKey() - i))
-                    .findFirst();
-            Optional<Integer> endWin = IntStream.range(0, entry.getKey())
-                    .boxed()
-                    .map(i -> entry.getKey() - i + 0 - 1) // Reversing: http://stackoverflow.com/a/24011264/2711488
-                    .filter(i -> entry.getValue() < i * (entry.getKey() - i))
-                    .findFirst();
-            if (startWin.isPresent() && endWin.isPresent()) {
-                result.updateAndGet(operand -> operand * ((endWin.get() - startWin.get()) + 1));
-            }
+            Optional<Integer> startWin = calculateRange(entry.getKey(), entry.getValue(),
+                    UnaryOperator.identity());
+            Optional<Integer> endWin = calculateRange(entry.getKey(), entry.getValue(),
+                    i -> entry.getKey() - i + 0 - 1); // Reversing: http://stackoverflow.com/a/24011264/2711488
+            result.updateAndGet(operand -> operand * ((endWin.orElse(0) - startWin.orElse(0)) + 1));
         });
         System.out.println("The multiplied result is: " + result.get());
     }
